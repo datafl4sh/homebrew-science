9a1d02b5e499ef8d550b0a21c0a7d41a7f55eaf4
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2016-10-19 12:24:24 +0200

    Description shortened

--- silo.rb
+++ silo.rb
@@ -1,5 +1,5 @@
 class Silo < Formula
-  desc "LLNL Silo library. The library allows to create databases for the visualization tool VisIt."
+  desc "LLNL Silo library. Allows to create databases for VisIt."
   homepage "https://wci.llnl.gov/simulation/computer-codes/silo"
   url "https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2-bsd.tar.gz"
   sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
